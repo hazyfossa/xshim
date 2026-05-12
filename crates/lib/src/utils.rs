6c1fb50e0dd68e15fdc64251ed682e6dba711063
hazyfossa <hazyfossa@gmail.com>
2026-05-12 23:35:46 +0500

    Rewrite fd passing

--- crates/lib/src/utils.rs
+++ crates/lib/src/utils.rs
@@ -1,155 +1,71 @@
-#[macro_export(local_inner_macros)]
-macro_rules! trait_alias {
-    ($vis:vis trait $name:ident = $($for:tt)*) => {
-        $vis trait $name: $($for)* {}
-        impl<T: $($for)*> $name for T {}
-    };
-}
-
 pub mod fd {
-    // Copyright 2021, The Android Open Source Project
-    // Copyright 2026, hazyfossa
-    //
-    // This code is based on the command-fds crate.
-    // Main changes are:
-    // 1) fd mappings are mostly valid by construction, we only resolve conflicts at runtime.
-    // 2) system calls are over rustix instead of nix
-    //
-    // Original license notice below:
-    //
-    // Licensed under the Apache License, Version 2.0 (the "License");
-    // you may not use this file except in compliance with the License.
-    // You may obtain a copy of the License at
-    //
-    //     http://www.apache.org/licenses/LICENSE-2.0
-    //
-    // Unless required by applicable law or agreed to in writing, software
-    // distributed under the License is distributed on an "AS IS" BASIS,
-    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    // See the License for the specific language governing permissions and
-    // limitations under the License.
+
     use std::{
-        io, mem,
-        ops::RangeInclusive,
+        mem::ManuallyDrop,
         os::{
-            fd::{AsRawFd, FromRawFd, OwnedFd, RawFd},
+            fd::{AsRawFd, OwnedFd, RawFd},
             unix::process::CommandExt,
         },
         path::PathBuf,
         process::Command,
     };
 
-    use eyre::{ContextCompat, Result};
     use rustix::io::FdFlags;
 
-    trait_alias!(pub trait FdSource = Iterator<Item = RawFd> + Send + Sync + 'static);
-
-    struct FdMapping {
-        parent_fd: OwnedFd,
-        child_fd: RawFd,
+    /// A context should be the only thing that maps FDs for the specific command
+    pub struct FdContext {
+        parent_fds: ManuallyDrop<Vec<OwnedFd>>,
+        is_current: bool,
     }
 
-    pub struct FdContext<T> {
-        free_fd_source: T,
-        mappings: Vec<FdMapping>,
-    }
-
-    pub type SimpleFdContext = FdContext<RangeInclusive<RawFd>>;
-
-    impl FdContext<RangeInclusive<RawFd>> {
-        pub fn new(capacity: i32) -> Self {
-            let capacity = capacity.checked_add(1).unwrap_or_else(|| {
-                panic!(
-                    "overflow: the limit for SimpleFdContext capacity is {}",
-                    i32::MAX - 1
-                )
-            });
-
-            Self::manual(3..=3 + capacity + 1)
+    // Drop only if not set on current, otherwise preserve fds
+    impl Drop for FdContext {
+        fn drop(&mut self) {
+            if !self.is_current {
+                unsafe {
+                    ManuallyDrop::drop(&mut self.parent_fds);
+                }
+            }
         }
     }
 
-    impl<T: FdSource> FdContext<T> {
-        /// free_fd_source should be an iterator yielding valid, unused FDs in the child.
-        ///
-        /// If you aren't doing any FD passing besides FdContext, specifying any range
-        /// beyond 0..=2 should be safe.
-        ///
-        /// Note that the range should always contain one more fd than you want to pass.
-        /// It will be used for reassigning in case of conflict.
-        /// Failure
-        pub fn manual(free_fd_source: T) -> Self {
+    impl FdContext {
+        pub fn new() -> Self {
             Self {
-                free_fd_source,
-                mappings: Vec::new(),
+                parent_fds: ManuallyDrop::new(Vec::new()),
+                is_current: false,
             }
         }
 
-        pub fn pass(&mut self, fd: OwnedFd) -> Result<PassedFd> {
-            let mapped_fd = self
-                .free_fd_source
-                .next()
-                .context("Free fd source exhausted")?;
-
-            self.mappings.push(FdMapping {
-                parent_fd: fd,
-                child_fd: mapped_fd,
-            });
-            Ok(PassedFd(mapped_fd))
-        }
-
-        /// This function does not allocate
-        fn apply(&mut self) -> io::Result<()> {
-            // NOTE: mappings are valid by linear construction from iterator
-
-            let safe_temporary_fd = self.free_fd_source.next().expect(
-                "Cannot assign a safe temporary fd: free_fd_source exhausted.
-                Potential conflict resolution will fail. Expand free_fd_source.",
-            );
-
-            let child_fds: Vec<RawFd> = self.mappings.iter().map(|m| m.child_fd).collect();
-
-            // Resolve conflicts between parent and child
-            for mapping in self.mappings.iter_mut() {
-                if child_fds.contains(&mapping.parent_fd.as_raw_fd())
-                    && mapping.parent_fd.as_raw_fd() != mapping.child_fd
-                {
-                    mapping.parent_fd =
-                        rustix::io::fcntl_dupfd_cloexec(&mapping.parent_fd, safe_temporary_fd)?;
-                }
-            }
-
-            for mapping in &self.mappings {
-                if mapping.child_fd == mapping.parent_fd.as_raw_fd() {
-                    // Remove the FD_CLOEXEC flag, so the FD will be kept open after exec.
-                    rustix::io::fcntl_setfd(&mapping.parent_fd, FdFlags::empty())?;
-                } else {
-                    // This closes child_fd if it is already open as something else, and clears the
-                    // FD_CLOEXEC flag on child_fd.
-
-                    // Safety:
-                    // fds from free_fd_source are guaranteed (by caller) to be unused
-                    // child_fd in each mapping is derived from free_fd_source
-                    // therefore, we have permission to treat any child_fd as Owned,
-                    // and close it if necessary
-                    let mut owned_projection = unsafe { OwnedFd::from_raw_fd(mapping.child_fd) };
-                    rustix::io::dup2(&mapping.parent_fd, &mut owned_projection)?;
-                    mem::forget(owned_projection);
-                }
-            }
-
-            Ok(())
+        pub fn pass(&mut self, fd: OwnedFd) -> PassedFd {
+            let ret = PassedFd(fd.as_raw_fd());
+            self.parent_fds.push(fd);
+            ret
         }
     }
 
+    // TODO: this can only be safely called once
     pub trait CommandFdExt {
-        fn with_fd_context<T: FdSource>(&mut self, fd_ctx: FdContext<T>) -> &mut Self;
+        fn with_fd_context(&mut self, ctx: FdContext) -> &mut Self;
     }
 
     impl CommandFdExt for Command {
-        fn with_fd_context<T: FdSource>(&mut self, mut fd_ctx: FdContext<T>) -> &mut Self {
-            // Safety: apply() does not allocate, rustix calls are safe
-            unsafe { self.pre_exec(move || fd_ctx.apply()) }
+        fn with_fd_context(&mut self, mut ctx: FdContext) -> &mut Self {
+            // Safety: the closure not allocate, rustix calls are safe
+            unsafe {
+                self.pre_exec(move || {
+                    for fd in &*ctx.parent_fds {
+                        // Remove the FD_CLOEXEC flag, so the FD will be kept open after exec.
+                        rustix::io::fcntl_setfd(&fd, FdFlags::empty())?;
+                    }
+
+                    ctx.is_current = true;
+
+                    Ok(())
+                })
+            };
+
+            self
         }
     }
 
